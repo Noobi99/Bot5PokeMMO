a5ee7c2be33550069cec4de411be1e93b100bc33
Noobi99 <chollensen1999@gmail.com>
2017-04-16 13:31:54 +0200

    Fixed TopMost by setting its property in Designer to checked = true

--- App.Designer.cs
+++ App.Designer.cs
@@ -60,14 +60,14 @@
             this.txtUp = new System.Windows.Forms.TextBox();
             this.btnHotkeys = new System.Windows.Forms.Button();
             this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.lblEncounters = new System.Windows.Forms.Label();
-            this.ofdScript = new System.Windows.Forms.OpenFileDialog();
-            this.chkMoveWin = new System.Windows.Forms.CheckBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtHeight = new System.Windows.Forms.TextBox();
-            this.lblHeight = new System.Windows.Forms.Label();
             this.lblWidth = new System.Windows.Forms.Label();
             this.txtWidth = new System.Windows.Forms.TextBox();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkMoveWin = new System.Windows.Forms.CheckBox();
+            this.lblEncounters = new System.Windows.Forms.Label();
+            this.ofdScript = new System.Windows.Forms.OpenFileDialog();
             this.panel1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
@@ -79,7 +79,7 @@
             // 
             this.btnStart.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnStart.Location = new System.Drawing.Point(418, 12);
-            this.btnStart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(2);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(87, 30);
             this.btnStart.TabIndex = 0;
@@ -96,7 +96,7 @@
             // 
             this.rtbConsole.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.rtbConsole.Location = new System.Drawing.Point(258, 30);
-            this.rtbConsole.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rtbConsole.Margin = new System.Windows.Forms.Padding(2);
             this.rtbConsole.Name = "rtbConsole";
             this.rtbConsole.ReadOnly = true;
             this.rtbConsole.Size = new System.Drawing.Size(295, 216);
@@ -109,8 +109,10 @@
             this.chkOnTop.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.chkOnTop.AutoSize = true;
+            this.chkOnTop.Checked = true;
+            this.chkOnTop.CheckState = System.Windows.Forms.CheckState.Checked;
             this.chkOnTop.Location = new System.Drawing.Point(20, 28);
-            this.chkOnTop.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.chkOnTop.Margin = new System.Windows.Forms.Padding(2);
             this.chkOnTop.Name = "chkOnTop";
             this.chkOnTop.Size = new System.Drawing.Size(68, 17);
             this.chkOnTop.TabIndex = 3;
@@ -146,7 +148,7 @@
             this.panel1.Controls.Add(this.btnBrowse);
             this.panel1.Controls.Add(this.btnStart);
             this.panel1.Location = new System.Drawing.Point(-1, 267);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(567, 58);
             this.panel1.TabIndex = 5;
@@ -165,7 +167,7 @@
             // 
             this.txtLoadedScript.BackColor = System.Drawing.Color.White;
             this.txtLoadedScript.Location = new System.Drawing.Point(32, 22);
-            this.txtLoadedScript.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtLoadedScript.Margin = new System.Windows.Forms.Padding(2);
             this.txtLoadedScript.Name = "txtLoadedScript";
             this.txtLoadedScript.ReadOnly = true;
             this.txtLoadedScript.Size = new System.Drawing.Size(290, 20);
@@ -174,7 +176,7 @@
             // btnBrowse
             // 
             this.btnBrowse.Location = new System.Drawing.Point(326, 12);
-            this.btnBrowse.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(2);
             this.btnBrowse.Name = "btnBrowse";
             this.btnBrowse.Size = new System.Drawing.Size(87, 30);
             this.btnBrowse.TabIndex = 4;
@@ -188,7 +190,7 @@
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Location = new System.Drawing.Point(9, 10);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(248, 253);
@@ -204,9 +206,9 @@
             this.tabPage1.Controls.Add(this.label2);
             this.tabPage1.Controls.Add(this.cmbWalk);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2);
             this.tabPage1.Size = new System.Drawing.Size(240, 227);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "General";
@@ -216,7 +218,7 @@
             // 
             this.chkCatchShiny.AutoSize = true;
             this.chkCatchShiny.Location = new System.Drawing.Point(13, 106);
-            this.chkCatchShiny.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.chkCatchShiny.Margin = new System.Windows.Forms.Padding(2);
             this.chkCatchShiny.Name = "chkCatchShiny";
             this.chkCatchShiny.Size = new System.Drawing.Size(194, 17);
             this.chkCatchShiny.TabIndex = 10;
@@ -247,7 +249,7 @@
             // txtSleepE
             // 
             this.txtSleepE.Location = new System.Drawing.Point(61, 73);
-            this.txtSleepE.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtSleepE.Margin = new System.Windows.Forms.Padding(2);
             this.txtSleepE.MaxLength = 4;
             this.txtSleepE.Name = "txtSleepE";
             this.txtSleepE.Size = new System.Drawing.Size(39, 20);
@@ -257,7 +259,7 @@
             // txtSleepB
             // 
             this.txtSleepB.Location = new System.Drawing.Point(12, 73);
-            this.txtSleepB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtSleepB.Margin = new System.Windows.Forms.Padding(2);
             this.txtSleepB.MaxLength = 4;
             this.txtSleepB.Name = "txtSleepB";
             this.txtSleepB.Size = new System.Drawing.Size(39, 20);
@@ -284,7 +286,7 @@
             "Square",
             "Perfect Square"});
             this.cmbWalk.Location = new System.Drawing.Point(12, 28);
-            this.cmbWalk.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cmbWalk.Margin = new System.Windows.Forms.Padding(2);
             this.cmbWalk.Name = "cmbWalk";
             this.cmbWalk.Size = new System.Drawing.Size(128, 21);
             this.cmbWalk.TabIndex = 4;
@@ -302,9 +304,9 @@
             this.tabPage2.Controls.Add(this.txtUp);
             this.tabPage2.Controls.Add(this.btnHotkeys);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2);
             this.tabPage2.Size = new System.Drawing.Size(240, 227);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Hotkeys";
@@ -353,7 +355,7 @@
             // txtRight
             // 
             this.txtRight.Location = new System.Drawing.Point(16, 83);
-            this.txtRight.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtRight.Margin = new System.Windows.Forms.Padding(2);
             this.txtRight.Name = "txtRight";
             this.txtRight.Size = new System.Drawing.Size(70, 20);
             this.txtRight.TabIndex = 9;
@@ -362,7 +364,7 @@
             // txtLeft
             // 
             this.txtLeft.Location = new System.Drawing.Point(16, 60);
-            this.txtLeft.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtLeft.Margin = new System.Windows.Forms.Padding(2);
             this.txtLeft.Name = "txtLeft";
             this.txtLeft.Size = new System.Drawing.Size(70, 20);
             this.txtLeft.TabIndex = 8;
@@ -371,7 +373,7 @@
             // txtDown
             // 
             this.txtDown.Location = new System.Drawing.Point(16, 37);
-            this.txtDown.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtDown.Margin = new System.Windows.Forms.Padding(2);
             this.txtDown.Name = "txtDown";
             this.txtDown.Size = new System.Drawing.Size(70, 20);
             this.txtDown.TabIndex = 7;
@@ -380,7 +382,7 @@
             // txtUp
             // 
             this.txtUp.Location = new System.Drawing.Point(16, 15);
-            this.txtUp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtUp.Margin = new System.Windows.Forms.Padding(2);
             this.txtUp.Name = "txtUp";
             this.txtUp.Size = new System.Drawing.Size(70, 20);
             this.txtUp.TabIndex = 6;
@@ -389,7 +391,7 @@
             // btnHotkeys
             // 
             this.btnHotkeys.Location = new System.Drawing.Point(16, 106);
-            this.btnHotkeys.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnHotkeys.Margin = new System.Windows.Forms.Padding(2);
             this.btnHotkeys.Name = "btnHotkeys";
             this.btnHotkeys.Size = new System.Drawing.Size(118, 23);
             this.btnHotkeys.TabIndex = 5;
@@ -408,66 +410,12 @@
             this.tabPage3.Controls.Add(this.chkMoveWin);
             this.tabPage3.Controls.Add(this.chkOnTop);
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
-            this.tabPage3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Size = new System.Drawing.Size(240, 227);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Advanced";
             this.tabPage3.UseVisualStyleBackColor = true;
-            // 
-            // lblEncounters
-            // 
-            this.lblEncounters.AutoSize = true;
-            this.lblEncounters.Location = new System.Drawing.Point(261, 248);
-            this.lblEncounters.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblEncounters.Name = "lblEncounters";
-            this.lblEncounters.Size = new System.Drawing.Size(73, 13);
-            this.lblEncounters.TabIndex = 7;
-            this.lblEncounters.Text = "Encounters: 0";
-            // 
-            // ofdScript
-            // 
-            this.ofdScript.FileName = "openFileDialog1";
-            this.ofdScript.FileOk += new System.ComponentModel.CancelEventHandler(this.ofdScript_FileOk);
-            // 
-            // chkMoveWin
-            // 
-            this.chkMoveWin.AutoSize = true;
-            this.chkMoveWin.Location = new System.Drawing.Point(20, 50);
-            this.chkMoveWin.Name = "chkMoveWin";
-            this.chkMoveWin.Size = new System.Drawing.Size(184, 17);
-            this.chkMoveWin.TabIndex = 12;
-            this.chkMoveWin.Text = "Move(0,0) and Resize PokeMMO";
-            this.chkMoveWin.UseVisualStyleBackColor = true;
-            this.chkMoveWin.CheckedChanged += new System.EventHandler(this.chkMoveWin_CheckedChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(17, 11);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 13);
-            this.label1.TabIndex = 13;
-            this.label1.Text = "Useful settings:";
-            // 
-            // txtHeight
-            // 
-            this.txtHeight.Location = new System.Drawing.Point(21, 86);
-            this.txtHeight.MaxLength = 4;
-            this.txtHeight.Name = "txtHeight";
-            this.txtHeight.Size = new System.Drawing.Size(100, 20);
-            this.txtHeight.TabIndex = 14;
-            this.txtHeight.Text = "900";
-            // 
-            // lblHeight
-            // 
-            this.lblHeight.AutoSize = true;
-            this.lblHeight.Location = new System.Drawing.Point(18, 70);
-            this.lblHeight.Name = "lblHeight";
-            this.lblHeight.Size = new System.Drawing.Size(38, 13);
-            this.lblHeight.TabIndex = 15;
-            this.lblHeight.Text = "Height";
             // 
             // lblWidth
             // 
@@ -487,6 +435,60 @@
             this.txtWidth.TabIndex = 16;
             this.txtWidth.Text = "600";
             // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(18, 70);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(38, 13);
+            this.lblHeight.TabIndex = 15;
+            this.lblHeight.Text = "Height";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(21, 86);
+            this.txtHeight.MaxLength = 4;
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 14;
+            this.txtHeight.Text = "900";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Useful settings:";
+            // 
+            // chkMoveWin
+            // 
+            this.chkMoveWin.AutoSize = true;
+            this.chkMoveWin.Location = new System.Drawing.Point(20, 50);
+            this.chkMoveWin.Name = "chkMoveWin";
+            this.chkMoveWin.Size = new System.Drawing.Size(184, 17);
+            this.chkMoveWin.TabIndex = 12;
+            this.chkMoveWin.Text = "Move(0,0) and Resize PokeMMO";
+            this.chkMoveWin.UseVisualStyleBackColor = true;
+            this.chkMoveWin.CheckedChanged += new System.EventHandler(this.chkMoveWin_CheckedChanged);
+            // 
+            // lblEncounters
+            // 
+            this.lblEncounters.AutoSize = true;
+            this.lblEncounters.Location = new System.Drawing.Point(261, 248);
+            this.lblEncounters.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblEncounters.Name = "lblEncounters";
+            this.lblEncounters.Size = new System.Drawing.Size(73, 13);
+            this.lblEncounters.TabIndex = 7;
+            this.lblEncounters.Text = "Encounters: 0";
+            // 
+            // ofdScript
+            // 
+            this.ofdScript.FileName = "openFileDialog1";
+            this.ofdScript.FileOk += new System.ComponentModel.CancelEventHandler(this.ofdScript_FileOk);
+            // 
             // App
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -499,7 +501,7 @@
             this.Controls.Add(this.rtbConsole);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "App";
             this.Text = "Bot5PokeMMO";
